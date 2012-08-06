6530b668e05debf7a3fcf87bb59499fb1cc5f2e1
kyler <taaking@163.com>
2012-08-06 16:24:08 +0800

    Update index.php
    添加缓存变量

--- index.php
+++ index.php
@@ -2,20 +2,24 @@
 
 
 /**
- * 设置源静态文件的根目录的URL地址
+ * 设置源静态文件的根目录的URL地址,请修改为需要加速的网站地址
  * */
 define('STATIC_URL','http://www.baidu.com/');
 
 /**
- * SAE storage的domain
+ * SAE storage的domain ,使用KVDB 做缓存层.该变量失效
  * */
 define('DOMAIN','cdn');
 
 /**
  * 空请求时是否显示文档
  * */
-define('WELCOME_DOC',TRUE);
+define('WELCOME_DOC',FALSE);
 
+/**
+ * 缓存有效期,秒数  一个月,由于失效机制不够实用.已经废弃
+ * */
+define('EXPIREDTIME',3600*24*30);
 /**
  * 运行环境:development/testing/production
  * */
